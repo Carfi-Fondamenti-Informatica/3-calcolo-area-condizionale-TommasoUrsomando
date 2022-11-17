860c9eb15f7192e343363829258dd29099df11c4
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-11-17 15:57:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+    float a, b;
+    int o;
+    cin >> a >> b >> o;
+    switch (o) {
+        case 0:
+            cout <<(a*b)/2;
+            break;
+        case 1:
+            cout << a*a;
+            break;
+        case 2:
+            cout << a*b;
+            break;
+        default:
+            cout << "opzione non valida";
+            break;
+    }
    return 0;
 }
